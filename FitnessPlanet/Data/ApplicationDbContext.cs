096466f35b2e41519e2e86cbe6f1d9e455c48d1c
Student <Student@DESKTOP-JRI0BF7>
2023-02-10 09:59:40 +0200

    Add project files.

---
+++ FitnessPlanet/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using FitnessPlanet.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessPlanet.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Manifacturer> Manifacturers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
---
+++ FitnessPlanet/Domain/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitnessPlanet.Domain
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string ProductName { get; set; }
+        public string Brand { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+
+        [Required]
+        public int ManifacturerId { get; set; }
+        public virtual Manifacturer Manifacturer { get; set; }
+        public string Picture { get; set; }
+        [Required]
+        [Range(0,5000)]
+        public int Quantity { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+        public virtual IEnumerable<Order> Orders { get; set; } = new List<Order>();
+    }
+}
