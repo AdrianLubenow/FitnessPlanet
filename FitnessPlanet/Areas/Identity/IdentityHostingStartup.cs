367de595cfc254cf11aeae6b4baa53e30598b70c
daddyAdrian <adrianlubenow@abv.bg>
2023-03-06 21:40:52 +0200

    Made a working login/register system and connected it to the database.

---
+++ FitnessPlanet/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FitnessPlanet.Data;
+using FitnessPlanet.Domain;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FitnessPlanet.Areas.Identity.IdentityHostingStartup))]
+namespace FitnessPlanet.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
--- FitnessPlanet/Startup.cs
+++ FitnessPlanet/Startup.cs
@@ -1,4 +1,5 @@
 using FitnessPlanet.Data;
+using FitnessPlanet.Domain;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -28,14 +29,18 @@ namespace FitnessPlanet
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+            options.UseLazyLoadingProxies()
+            .UseSqlServer(
+                Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddDefaultIdentity<ApplicationUser>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
             services.AddControllersWithViews();
-            services.AddRazorPages();
+            //services.AddRazorPages();
             services.Configure<IdentityOptions>(option =>
             {
                 option.Password.RequireDigit = false;
